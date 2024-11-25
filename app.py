0fcfb06f931ed93ea50feeb35fb73648183bf95b
PolinaZaiko <zpolina2003@mail.ru>
2024-11-25 16:16:18 +0700

    Изменение ошибки

--- app.py
+++ app.py
@@ -4,20 +4,17 @@ from flasgger import Swagger
 app = Flask(__name__)
 swagger = Swagger(app)
 
-# Хранилище контактов
 contacts = {}
 contact_id_counter = 1
 
 
 @app.route('/')
 def index():
-    """Главная страница с отображением всех контактов"""
     return render_template('index.html', contacts=contacts.values())
 
 
 @app.route('/contacts/new', methods=['GET', 'POST'])
 def create_contact():
-    """Создание нового контакта"""
     global contact_id_counter
     if request.method == 'POST':
         data = request.form
@@ -25,27 +22,25 @@ def create_contact():
         contacts[contact_id] = {
             "id": contact_id,
             "name": data['name'],
-            "phone": data['phone']
+            "phone": data['phone'],
         }
         contact_id_counter += 1
         return redirect(url_for('index'))
-    
+
     return render_template('create_contact.html')
 
 
 @app.route('/contacts/<int:id>', methods=['GET'])
 def get_contact(id):
-    """Получение информации о контакте по его идентификатору"""
     contact = contacts.get(id)
     if not contact:
         return jsonify({"message": "Contact not found"}), 404
-    
+
     return render_template('contact.html', contact=contact)
 
 
 @app.route('/contacts/<int:id>', methods=['POST'])
 def delete_contact(id):
-    """Удаление контакта по его идентификатору"""
     if id not in contacts:
         return jsonify({"message": "Contact not found"}), 404
 
