42d38a634de98091cb6948c15f7a14d460b302a2
PolinaZaiko <zpolina2003@mail.ru>
2024-11-27 15:29:32 +0700

    Изменила файл

--- app.py
+++ app.py
@@ -9,10 +9,27 @@ contact_id_counter = 1
 
 @app.route('/')
 def index():
+    """Главная страница с списком контактов."""
     return render_template('index.html', contacts=contacts.values())
 
 @app.route('/contacts/new', methods=['GET', 'POST'])
 def create_contact():
+    """
+    Создание нового контакта.
+    ---
+    parameters:
+      - name: name
+        type: string
+        required: true
+        description: Имя контакта.
+      - name: phone
+        type: string
+        required: true
+        description: Номер телефона.
+    responses:
+      302:
+        description: Контакт успешно создан и перенаправление на главную страницу.
+    """
     global contact_id_counter
     if request.method == 'POST':
         data = request.form
@@ -29,6 +46,20 @@ def create_contact():
 
 @app.route('/contacts/<int:id>', methods=['GET'])
 def get_contact(id):
+    """
+    Получение информации о контакте по его идентификатору.
+    ---
+    parameters:
+      - name: id
+        type: integer
+        required: true
+        description: Идентификатор контакта.
+    responses:
+      200:
+        description: Информация о контакте.
+      404:
+        description: Контакт не найден.
+    """
     contact = contacts.get(id)
     if not contact:
         return jsonify({"message": "Contact not found"}), 404
@@ -37,6 +68,20 @@ def get_contact(id):
 
 @app.route('/contacts/<int:id>', methods=['POST'])
 def delete_contact(id):
+    """
+    Удаление контакта по его идентификатору.
+    ---
+    parameters:
+      - name: id
+        type: integer
+        required: true
+        description: Идентификатор контакта.
+    responses:
+      302:
+        description: Успешное удаление контакта и перенаправление на главную страницу.
+      404:
+        description: Контакт не найден.
+    """
     if id not in contacts:
         return jsonify({"message": "Contact not found"}), 404
 
