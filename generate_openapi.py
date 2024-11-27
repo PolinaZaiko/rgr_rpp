fafa380a89e4df02e2b4b20b6e685418caa3e146
PolinaZaiko <zpolina2003@mail.ru>
2024-11-27 18:49:52 +0700

    Добавила

--- generate_openapi.py
+++ generate_openapi.py
@@ -3,23 +3,27 @@ import os
 
 from app import app
 
+
 def generate_openapi_yaml():
+    docs_dir = "docs"
+    os.makedirs(docs_dir, exist_ok=True)
+
     with app.test_client() as client:
         response = client.get('/apispec_1.json')
         
         if response.status_code == 200 and response.is_json:
             openapi_spec = response.json
+            
+            openapi_file_path = os.path.join(docs_dir, "openapi.yaml")
 
-            # Создаем директорию docs, если она не существует
-            os.makedirs("docs", exist_ok=True)
-
-            with open("docs/openapi.yaml", "w") as file:
+            with open(openapi_file_path, "w") as file:
                 yaml.dump(openapi_spec, file, default_flow_style=False)
             
             print("Спецификация OpenAPI сохранена в docs/openapi.yaml")
         else:
             print("Ошибка: не удалось получить спецификацию OpenAPI.")
             print(response.status_code, response.data)
+
 
 if __name__ == "__main__":
     generate_openapi_yaml()
