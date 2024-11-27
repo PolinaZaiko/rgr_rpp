7a2361379dea455504a06ed2d0bc9186500f159c
PolinaZaiko <zpolina2003@mail.ru>
2024-11-27 17:32:12 +0700

    Добавила

--- generate_openapi.py
+++ generate_openapi.py
@@ -6,16 +6,20 @@ from app import app
 
 def generate_openapi_yaml():
 
+    os.makedirs("docs", exist_ok=True)
+
     with app.test_client() as client:
         response = client.get('/apispec_1.json')
         
         if response.status_code == 200 and response.is_json:
             openapi_spec = response.json
 
-            with open("docs/openapi.yaml", "w") as file:
+            openapi_file_path = os.path.join("docs", "openapi.yaml")
+
+            with open(openapi_file_path, "w") as file:
                 yaml.dump(openapi_spec, file, default_flow_style=False)
             
-            print("Спецификация OpenAPI сохранена в docs/openapi.yaml")
+            print("Спецификация OpenAPI сохранена в {openapi_file_path}")
         else:
             print("Ошибка: не удалось получить спецификацию OpenAPI.")
             print(response.status_code, response.data)
